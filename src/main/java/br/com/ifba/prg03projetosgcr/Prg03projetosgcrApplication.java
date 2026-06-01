3bbf487819380efd3ab33890414350047329170d
João Luis <joaoluislopesaraujo088@gmail.com>
2026-06-01 18:11:52 -0300

    chore: inicializar estrutura base do projeto com Spring Boot
    - Adicionados arquivos de configuração inicial gerados pelo Spring Initializr.
    
    - Dependencies: Lombok e Spring Data

---
+++ src/main/java/br/com/ifba/prg03projetosgcr/Prg03projetosgcrApplication.java
@@ -0,0 +1,14 @@
+package br.com.ifba.prg03projetosgcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prg03projetosgcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prg03projetosgcrApplication.class, args);
+                System.out.println("Hello World");
+	}
+
+}
